3ac9d065a6f96a4c9dff87a56fc0cf9d2c76d1a9
Dana Yatsuta <danayatsuta@gmail.com>
2025-02-21 06:49:01 +0300

    Add background

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,7 @@
 <template>
-  <div class="font-ubuntu"></div>
+  <div
+    class="font-ubuntu bg-magnolia min-h-screen bg-[url(./img/bg-sidebar-mobile.svg)] bg-no-repeat"
+  >
+    <div class="absolute right-0 bottom-0 left-0 h-18 bg-white"></div>
+  </div>
 </template>
