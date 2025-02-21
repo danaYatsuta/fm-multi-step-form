84fd4881a3c85ca03c08b5cc775c6c35a460aeb0
Dana Yatsuta <danayatsuta@gmail.com>
2025-02-21 07:21:15 +0300

    Add step buttons

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,28 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import StepButton from './components/StepButton.vue'
+
+const currentStep = ref(1)
+const lastCompletedStep = ref(1)
+
+function onStepButtonClick(step: number) {
+  if (step <= lastCompletedStep.value) currentStep.value = step
+}
+</script>
+
 <template>
   <div
-    class="font-ubuntu bg-magnolia min-h-screen bg-[url(./img/bg-sidebar-mobile.svg)] bg-no-repeat"
+    class="font-ubuntu bg-magnolia flex min-h-screen flex-col bg-[url(./img/bg-sidebar-mobile.svg)] bg-no-repeat"
   >
+    <nav class="mt-8 flex gap-4 self-center">
+      <StepButton
+        v-for="step in 4"
+        :key="step"
+        :number="step"
+        :active="step == currentStep"
+        @click="onStepButtonClick(step)"
+      />
+    </nav>
     <div class="absolute right-0 bottom-0 left-0 h-18 bg-white"></div>
   </div>
 </template>
---
+++ src/components/StepButton.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps<{
+  number: number
+  active?: boolean
+}>()
+</script>
+
+<template>
+  <button
+    type="button"
+    class="flex h-8 w-8 items-center justify-center rounded-full pb-px text-sm font-bold"
+    :class="{ 'bg-light-blue text-marine-blue': active, 'border text-white': !active }"
+  >
+    {{ number }}
+  </button>
+</template>
