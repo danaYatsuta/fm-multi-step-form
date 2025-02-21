e76244384c9ed3f41abdbcaca3ac8367e5760558
Dana Yatsuta <danayatsuta@gmail.com>
2025-02-21 07:56:56 +0300

    Add form fields

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import StepButton from './components/StepButton.vue'
+import FormTextInput from './components/FormTextInput.vue'
 
 const currentStep = ref(1)
 const lastCompletedStep = ref(1)
@@ -26,7 +27,20 @@ function onStepButtonClick(step: number) {
 
     <main class="mx-4 rounded-lg bg-white px-6 py-8 shadow-lg">
       <h1 class="text-marine-blue text-2xl font-bold">Personal info</h1>
+
       <p class="text-cool-gray mt-2">Please provide your name, email address, and phone number.</p>
+
+      <form class="mt-4 flex flex-col gap-3" @submit.prevent>
+        <FormTextInput id="name" placeholder="e.g. Stephen King">Name</FormTextInput>
+        <FormTextInput id="email" placeholder="e.g. stephenking@lorem.com"
+          >Email Address</FormTextInput
+        >
+        <FormTextInput id="phone_number" placeholder="e.g. +1 234 567 890"
+          >Phone Number</FormTextInput
+        >
+
+        <button type="submit" class="hidden" aria-label="Next Step"></button>
+      </form>
     </main>
 
     <div class="absolute right-0 bottom-0 left-0 h-18 bg-white"></div>
---
+++ src/components/FormTextInput.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+defineOptions({
+  inheritAttrs: false,
+})
+
+defineProps<{
+  id: string
+}>()
+
+const model = defineModel<string>()
+</script>
+
+<template>
+  <div class="text-marine-blue flex flex-col gap-0.5">
+    <label :for="id" class="text-xs"><slot /></label>
+    <input
+      :id
+      v-model="model"
+      type="text"
+      v-bind="$attrs"
+      class="border-light-gray placeholder:text-cool-gray focus:border-purplish-blue h-10 rounded-sm border px-3 text-sm font-medium outline-none"
+    />
+  </div>
+</template>
